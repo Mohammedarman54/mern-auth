535e9249aa646395e1fa63a1f11bb7dd8c8548cb
Mohammed Arman Ali <151983300+Mohammedarman54@users.noreply.github.com>
2025-09-18 23:31:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -43,4 +43,4 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
 // Start server ✅ FIXED
-app.listen(port, () => console.log(Server started on port : ${port}));
+app.listen(port, () => console.log(`Server started on port : ${port}`));
